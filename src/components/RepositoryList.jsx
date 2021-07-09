7af85b47a5c75af9cd5547870714597612c0b001
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-09 19:00:54 -0300

    Adding RepositoryList component file

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+export function RepositoryList () {
+    return (
+        <section className="repository-list">
+            <h1>Lista de Respositório</h1>
+
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
