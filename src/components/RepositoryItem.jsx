6c2a155ca5217398e8fd9e86b56b5d963678eb48
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-10 01:13:22 -0300

    Renaming RepositoryItem component and adding types

--- src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props) {
-    return (
-        <li>
-            <strong>{props.repository ?? "Default"}</strong>
-            <p>{props.description}</p>
-            <a href={props.link}>Acessar Repositório</a>
-        </li>
-    )
-}
