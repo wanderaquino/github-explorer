a11fe32c9260a30f98360a8e4472cab312a09d3d
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-09 23:15:46 -0300

    Adding useEffect for get repositories API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,5 @@
 import {RepositoryItem} from "./RepositoryItem";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import "../style/repositories.scss";
 
 const repositoryData = {
@@ -10,7 +10,13 @@ const repositoryData = {
 
 export function RepositoryList () {
     const [repositoryList, setRepositoryList] = useState([]);
-    
+
+    useEffect(() => {
+        fetch("https://api.github.com/orgs/rocketseat/repos")
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de Respositórios</h1>
