32fe03e1b8388a70b995f1869dc877449c7da25c
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-10 00:43:02 -0300

    Removing unused components and adding useState for github API response

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,28 +2,26 @@ import {RepositoryItem} from "./RepositoryItem";
 import {useEffect, useState} from "react";
 import "../style/repositories.scss";
 
-const repositoryData = {
-    repository: "Unform 01",
-    description: "Forms in React",
-    link: "https://github.com"
-}
-
 export function RepositoryList () {
     const [repositoryList, setRepositoryList] = useState([]);
 
     useEffect(() => {
         fetch("https://api.github.com/orgs/rocketseat/repos")
             .then(response => response.json())
-            .then(data => console.log(data));
-    }, []);
+            .then(data => setRepositoryList(data));
+            }, []);
 
     return (
         <section className="repository-list">
             <h1>Lista de Respositórios</h1>
             <ul>
-                <RepositoryItem {... repositoryData}/>
-                <RepositoryItem {... repositoryData}/>
-                <RepositoryItem {... repositoryData}/>
+                {repositoryList.map(repo => 
+                <RepositoryItem 
+                    key={repo.name} 
+                    repository={repo.name} 
+                    description={repo.description} 
+                    link={repo.html_url} 
+                />)}    
             </ul>
         </section>
     )
