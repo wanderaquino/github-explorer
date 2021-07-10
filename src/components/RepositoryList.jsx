6490719e6731fe4c8b7cf0f9f8c4dbd38fdc5c23
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-09 21:27:04 -0300

    Rendering repository item on repository list component

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,24 +1,19 @@
+import {RepositoryItem} from "./RepositoryItem";
+
+const repositoryData = {
+    repository: "Unform 01",
+    description: "Forms in React",
+    link: "https://github.com"
+}
+
 export function RepositoryList () {
     return (
         <section className="repository-list">
-            <h1>Lista de Respositório</h1>
-
+            <h1>Lista de Respositórios</h1>
             <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar Repositório</a>
-                </li>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar Repositório</a>
-                </li>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar Repositório</a>
-                </li>
+                <RepositoryItem {... repositoryData}/>
+                <RepositoryItem {... repositoryData}/>
+                <RepositoryItem {... repositoryData}/>
             </ul>
         </section>
     )
